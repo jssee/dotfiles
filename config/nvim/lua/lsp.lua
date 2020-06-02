52760792f33a6cc62791d0c00e909c37f32a0d09
Jesse Hoyos <jesse@mojotech.com>
2020-06-02 01:33:28 -0400

    more lsp config

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,6 +1,15 @@
 vim.cmd('packadd nvim-lsp')
 local nvim_lsp = require'nvim_lsp'
 
+vim.fn.sign_define('LspDiagnosticsErrorSign', {text='✖ ' or 'E', texthl='LspDiagnosticsError', linehl='', numhl=''})
+vim.fn.sign_define('LspDiagnosticsWarningSign', {text='⚠' or 'W', texthl='LspDiagnosticsWarning', linehl='', numhl=''})
+vim.fn.sign_define('LspDiagnosticsInformationSign', {text='ℹ' or 'I', texthl='LspDiagnosticsInformation', linehl='', numhl=''})
+vim.fn.sign_define('LspDiagnosticsHintSign', {text='➤' or 'H', texthl='LspDiagnosticsHint', linehl='', numhl=''})
+
+vim.api.nvim_command('highlight! link LspDiagnosticsError DiffDelete')
+vim.api.nvim_command('highlight! link LspDiagnosticsWarning DiffChange')
+vim.api.nvim_command('highlight! link LspDiagnosticsHint Comment')
+
 local on_attach = function(client, bufnr)
   -- set the omnifunction to use the lsp
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -15,6 +24,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ld', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gq', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
 end
 
 local servers = {
